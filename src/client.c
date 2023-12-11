009c0c93c21b35948819809d63aa0892ea495053
AllanDantas21 <Allandantas21.ad@gmail.com>
2023-12-11 14:52:20 -0300

    add pid_invalid function handler

--- src/client.c
+++ src/client.c
@@ -21,10 +21,26 @@ void	send_msg(int id, char *msg)
 
 	i = 0;
 	while(msg[i])
-	{
+	{	
 		handle_char(id, msg[i++]);
 	}
 	handle_char(id, '\n');
+}
+
+int	validate_args(char *pid, char *msg_arg)
+{
+	if (!msg_arg)
+		return (-1);
+	while(*pid)
+	{
+		if(!ft_isdigit(*pid))
+		{
+			ft_printf("o pid só deve conter digitos!");
+			return (-1);
+		}
+		pid++;
+	}
+	return (0);
 }
 
 int main(int argc, char **argv)
@@ -32,6 +48,8 @@ int main(int argc, char **argv)
 	int	id;
 	if (argc == 3)
 	{
+		if(validate_args(argv[1], argv[2]) == -1)
+			return (0);
 		id = ft_atoi(argv[1]);	
 		send_msg(id, argv[2]);
 	}
