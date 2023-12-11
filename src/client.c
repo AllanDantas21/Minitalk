bbd44f0d672bb41f182f765eb16901e94fd4ef84
AllanDantas21 <Allandantas21.ad@gmail.com>
2023-12-11 14:55:37 -0300

    change name pid_invalid function handler

--- src/client.c
+++ src/client.c
@@ -27,7 +27,7 @@ void	send_msg(int id, char *msg)
 	handle_char(id, '\n');
 }
 
-int	validate_args(char *pid, char *msg_arg)
+int	check_args(char *pid, char *msg_arg)
 {
 	if (!msg_arg)
 		return (-1);
@@ -48,7 +48,7 @@ int main(int argc, char **argv)
 	int	id;
 	if (argc == 3)
 	{
-		if(validate_args(argv[1], argv[2]) == -1)
+		if(check_args(argv[1], argv[2]) == -1)
 			return (0);
 		id = ft_atoi(argv[1]);	
 		send_msg(id, argv[2]);
