6622a9d6138a43872d99cb8b5025212c46268bfa
AllanDantas21 <Allandantas21.ad@gmail.com>
2023-12-13 03:01:31 -0300

    add bonus files

---
+++ src/client_bonus.c
@@ -0,0 +1,74 @@
+#include "../include/minitalk.h"
+
+static void sig_ack(int sig)
+{
+    if (sig == SIGUSR1)
+        ft_printf("Mensagem recebida!");
+    else
+        ft_printf("Mensagem recebida!");
+
+}
+
+void	char_to_sig(int pid, char character)
+{
+	int	i;
+
+	i = 8;
+	while (i--)
+	{
+		if (character & (1 << i))
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		usleep(100);
+	}
+}
+
+void	send_msg(int id, char *msg)
+{
+	int	i;
+
+	i = 0;
+	while (msg[i])
+	{
+		char_to_sig(id, msg[i++]);
+	}
+	char_to_sig(id, '\n');
+}
+
+int	check_args(char *pid, char *msg_arg)
+{
+	if (!msg_arg)
+		return (-1);
+	while (*pid)
+	{
+		if (!ft_isdigit(*pid))
+		{
+			ft_printf("o pid só deve conter digitos!");
+			return (-1);
+		}
+		pid++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	int	id;
+
+	if (argc == 3)
+	{
+		if (check_args(argv[1], argv[2]) == -1)
+			return (0);
+        signal(SIGUSR1, sig_ack);
+        signal(SIGUSR2, sig_ack);
+		id = ft_atoi(argv[1]);
+		send_msg(id, argv[2]);
+	}
+	else
+	{
+		ft_printf("Erro, os argumentos de entrada estão errados!\n");
+		ft_printf("os argumentos devem ser: [./client PID mensagem]\n");
+	}
+	return (0);
+}
---
+++ src/server_bonus.c
@@ -0,0 +1,51 @@
+#include "../include/minitalk.h"
+
+void	minitalk_header(void)
+{
+	/*ft_printf("\033[32m _____ _     _ _       _ _    \n");
+	ft_printf(" |     |_|___|_| |_ ___| | |_ \n");
+	ft_printf(" | | | | |   | |  _| .'| | '_|\n");
+	ft_printf(" |_|_|_|_|_|_|_|_| |__,|_|_,_|\n\033[37m");*/
+	ft_printf("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣥⣤⣾⠟⡛⠿⠿⣭⣻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⣿⣿⣿⡟⣿⣽⡟⡏⢩⣦⡝⠋⢸⣶⠄⢲⡟⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⣿⣿⣯⣷⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣌⡳⣜⢿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢀⡛⢌⢿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠄⠙⠌⣸⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⡿⠉⠉⠉⠉⢿⣿⣿⣿⠏⠉⠉⠉⠉⠉⠆⠄⠁⠄⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⡗⠫⠿⠆⠄⠸⢿⣿⣿⠂⠒⠲⡿⠛⠛⠂⠄⠄⢠⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⡛⣧⡔⠢⠴⣃⣠⣼⣿⣧⡀⠘⢢⣀⠄⠄⠄⠄⠄⢈⠁⢿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣿⠄⠄⠄⣸⠆⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣼⢿⣿⣿⣿⣿⡀⠄⠘⡀⢠⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⡌⠿⣫⣿⣦⠬⢭⣥⣶⣬⣾⣿⢿⣿⡟⠄⢀⣿⣶⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⣧⠘⣉⠛⢻⣛⣛⣛⣻⡶⠮⠙⠃⣉⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⣿⡆⠸⣿⣶⢾⣿⣯⣤⣄⣀⣾⡟⠄⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿\n⠟⠿⠿⠿⠿⢿⣷⠄⣿⣿⣎⣹⢻⣿⣿⡿⡿⠁⠄⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿\n⠄⠄⠄⠄⠄⠄⠄⣠⠘⣿⣿⣿⣿⣿⣿⡟⠁⣀⣀⣀⠄⠘⠿⣿⣿⣿⣿⣿⣿⣿\n");
+}
+
+void	sig_to_char(int sig)
+{
+	static int				i = 0;
+	static unsigned char	c = 0;
+
+	c |= (sig == SIGUSR2);
+	if (++i == 8)
+	{
+		ft_printf("%c", c);
+		i = 0;
+		c = 0;
+        kill(info->si_pid, SIGUSR2);
+	}
+	else
+		c <<= 1;
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argv;
+    struct sigaction    sig;
+
+	if (argc != 1)
+		ft_printf("Erro: Formato invalido\nTente: ./server\n");
+	else
+	{
+		minitalk_header();
+		ft_printf("server iniciado!!->");
+		ft_printf(" PID: %d\n", getpid());
+        sig.sa.sa_sigaction = sig_to_char;
+        sigemptyset(&sig.sa_mask);
+        sig.sa_flags = SA_SIGINFO;
+		while (1)
+		{
+			sigaction(SIGUSR1, &sig, NULL);
+			sigaction(SIGUSR2, &sig, NULL);
+		}
+	}   
+	return (0);
+}
